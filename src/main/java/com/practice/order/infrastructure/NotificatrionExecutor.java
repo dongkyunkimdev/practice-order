805a1e944955d6e5f3fd14f63fd66e5bfe4a3025
dongkyunkimdev <ddkds66@gmail.com>
2022-07-12 23:07:00 +0900

    feat: 파트너 application layer 구현
    - facade 패턴을 구현하여 여러 인터페이스를 조립

---
+++ src/main/java/com/practice/order/infrastructure/NotificatrionExecutor.java
@@ -0,0 +1,26 @@
+package com.practice.order.infrastructure;
+
+import com.practice.order.domain.notification.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class NotificatrionExecutor implements NotificationService {
+
+    @Override
+    public void sendEmail(String email, String title, String description) {
+        log.info("sendEmail");
+    }
+
+    @Override
+    public void sendKakao(String phoneNo, String description) {
+        log.info("sendKakao");
+    }
+
+    @Override
+    public void sendSms(String phoneNo, String description) {
+        log.info("sendSms");
+    }
+
+}

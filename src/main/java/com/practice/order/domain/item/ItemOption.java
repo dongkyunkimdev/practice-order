b34bf8e5ee5092dc321b5cfa4da29663b16cca2e
dongkyunkim <49021557+dongkyunkimdev@users.noreply.github.com>
2022-07-17 17:44:39 +0900

    feat: Item aggregate 엔티티 구현 (#12)

---
+++ src/main/java/com/practice/order/domain/item/ItemOption.java
@@ -0,0 +1,58 @@
+package com.practice.order.domain.item;
+
+import com.practice.order.common.exception.InvalidParamException;
+import com.practice.order.domain.AbstractEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "item_options")
+public class ItemOption extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_option_group_id")
+    @NotNull
+    private ItemOptionGroup itemOptionGroup;
+
+    @Column(name = "ordering")
+    @NotNull
+    private Integer ordering;
+
+    @Column(name = "item_option_name")
+    @NotNull
+    private String itemOptionName;
+
+    @Column(name = "item_option_price")
+    @NotNull
+    private Long itemOptionPrice;
+
+    public ItemOption(
+            ItemOptionGroup itemOptionGroup,
+            Integer ordering,
+            String itemOptionName,
+            Long itemOptionPrice
+    ) {
+        if (itemOptionGroup == null) throw new InvalidParamException("itemOption.itemOptionGroup");
+        if (ordering == null) throw new InvalidParamException("itemOption.ordering");
+        if (StringUtils.isBlank(itemOptionName)) throw new InvalidParamException("itemOption.itemOptionName");
+        if (itemOptionPrice == null) throw new InvalidParamException("itemOption.itemOptionPrice");
+
+        this.itemOptionGroup = itemOptionGroup;
+        this.ordering = ordering;
+        this.itemOptionName = itemOptionName;
+        this.itemOptionPrice = itemOptionPrice;
+    }
+}
---
+++ src/main/java/com/practice/order/domain/item/ItemOptionGroup.java
@@ -0,0 +1,52 @@
+package com.practice.order.domain.item;
+
+import com.google.common.collect.Lists;
+import com.practice.order.common.exception.InvalidParamException;
+import com.practice.order.domain.AbstractEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Slf4j
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "item_option_groups")
+public class ItemOptionGroup extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    @NotNull
+    private Item item;
+
+    @Column(name = "ordering")
+    @NotNull
+    private Integer ordering;
+
+    @Column(name = "item_option_group_name")
+    @NotNull
+    private String itemOptionGroupName;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "itemOptionGroup", cascade = CascadeType.PERSIST)
+    private List<ItemOption> itemOptionList = Lists.newArrayList();
+
+    public ItemOptionGroup(Item item, Integer ordering, String itemOptionGroupName) {
+        if (item == null) throw new InvalidParamException("ItemOptionGroup.item");
+        if (ordering == null) throw new InvalidParamException("ItemOptionGroup.item");
+        if (StringUtils.isBlank(itemOptionGroupName)) throw new InvalidParamException("ItemOptionGroup.item");
+
+        this.item = item;
+        this.ordering = ordering;
+        this.itemOptionGroupName = itemOptionGroupName;
+    }
+}
